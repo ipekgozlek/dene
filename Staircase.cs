ad89b51d93a54344736e1043ebbff26f20606888
ipek gozlek <ipekgg6@gmail.com>
2024-05-21 01:25:25 +0300

    first commit

---
+++ Staircase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Result
+{
+    public static void staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+            {
+                Console.Write(" ");
+            }
+
+            for (int k = 0; k < i; k++)
+            {
+                Console.Write("#");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Result.staircase(n);
+    }
+}
